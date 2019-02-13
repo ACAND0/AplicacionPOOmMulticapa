41627f76753b4deaf2cc88c889a248c3189a4e78
adria <adria@Adry>
2019-02-13 18:04:51 +0100

    v3.1 Paginación de departamentos estable funciona con criterios de búsqueda y botones con innactividad según la página Mto Usuarios sin paginar

--- controller/cMtoDepartamentos.php
+++ controller/cMtoDepartamentos.php
@@ -7,18 +7,20 @@
  * @version 2.6
  * @package controller
  */
-$contador = 0; //Variable que utilizao para concatenar en el name de cada departamento para así
-//poder referenciarlo y distinguirlo a la hora de utilizar los botones individuales de cada departamento como
-//el de borrar
-$registrosPorPagina = 3; //Número de registros mostrados por página
+
 if (isset($_REQUEST['Salir'])) {//Si hemos pulsado salir
     $_SESSION['pagina'] = 'inicio';  //Vaciamos la variable SESSION del usuario
+    //Al salir elimino las variables de busqueda
+     $_SESSION['criterioBusqueda'] = "Todos"; //Limpiamos el criterio de búsqueda
+    $_SESSION['buscarPorDesc'] = "";//Limpiamos la descripción
     header("Location: index.php"); //Y redireccionamos al index
     exit;
 }
 
 if (isset($_REQUEST['limpiarBuscar'])) {//Si hemos pulsado salir
     $_SESSION['pagina'] = 'mtoDepartamentos';  //Vaciamos la variable SESSION del usuario
+    $_SESSION['criterioBusqueda'] = "Todos"; //Limpiamos el criterio de búsqueda
+    $_SESSION['buscarPorDesc'] = "";//Limpiamos la descripción
     header("Location: index.php"); //Y redireccionamos al index
     exit;
 }
@@ -42,41 +44,8 @@ if (isset($_REQUEST['Añadir'])) {//Si hemos pulsado salir
 }
 
 
-
-//Abro la tabla que contendrá los datos de los departamentos en la vista
-$criterioBusqueda = "Todos"; //Valor por defecto si no se ha pulsado buscar
-$descripcion = ""; //Valor por defecto si no se ha pulsado buscar
-//*****************************************DATOS SOBRE PAGINACIÓN************************//
-//Recojo la página indicada por el usuario
-if (isset($_REQUEST["pag"])) {
-    $pagina = $_REQUEST["pag"];//Si se le ha indicado se asigna a la variable pagina el número de página
-} else {
-    $pagina = 1;//Si no se ha establecido ninguna página, se establece la página en 1 dando por hecho que el usuario no ha seleccionado ninguna página
-
-}
-
-
-
-if (isset($_REQUEST['Buscar'])) {
-    $criterioBusqueda = $_REQUEST['criterioBusqueda'];
-    $descripcion = $_REQUEST['buscarPorDesc'];
-}
-
-$registrosPorPagina = 3; //Registros a mostrar por página
-
-$numeroFilas = Departamento::contarDepartamentosPorDesc($descripcion, $criterioBusqueda); //Número de filas
-$totalPaginas = ceil($numeroFilas / $registrosPorPagina); //Número de páginas
-$primerRegistro = ($pagina - 1) * $registrosPorPagina; //Registro por el cual empieza a mostrar la página
-
-
-//echo "<br>".$primerRegistro."<br>";
-//echo $registrosPorPagina."<br>";
-
-$aDepartamentos = Departamento::buscaDepartamentosPorDescripcion($descripcion, $criterioBusqueda, $primerRegistro, $registrosPorPagina); //Devuelve array de objetos
-
-
-foreach ($aDepartamentos as $key => $Departamento) {//Recorro los departamentos teniendo en cuenta su índice
-       $key = $key + $primerRegistro; //Con esto asigno al key su valor actual más el de la propia página
+foreach ($_SESSION['aDepartamentos'] as $key => $Departamento) {//Recorro los departamentos teniendo en cuenta su índice
+       //$key = $key + $primerRegistro; //Con esto asigno al key su valor actual más el de la propia página
 
 //    echo 'Borrar' . $key;
     if (isset($_REQUEST['Borrar' . $key])) {//Si se ha pulsado el botón del departamento actual
@@ -108,6 +77,29 @@ foreach ($aDepartamentos as $key => $Departamento) {//Recorro los departamentos 
     }
 }
 
+
+
+if (isset($_REQUEST['Buscar'])) {//Si se ha pulsado buscar establezco los valores recogiéndolos
+    $_SESSION['criterioBusqueda'] = $_REQUEST['criterioBusqueda'];//Guardamos el criterio de búsqueda en la sesión para que se mantenga
+    $_SESSION['buscarPorDesc'] = $_REQUEST['buscarPorDesc'];//Guardamos la búsqueda por descripción en la sesión para que se mantenga
+}
+
+//*****************************************DATOS SOBRE PAGINACIÓN************************//
+
+//Recojo la página indicada por el usuario
+if (isset($_REQUEST["pag"])) {
+    $pagina = $_REQUEST["pag"];//Si se le ha indicado se asigna a la variable pagina el número de página
+} else {
+    $pagina = 1;//Si no se ha establecido ninguna página, se establece la página en 1 dando por hecho que el usuario no ha seleccionado ninguna página
+}
+
+$registrosPorPagina = 3; //Registros a mostrar por página
+$numeroFilas = Departamento::contarDepartamentosPorDesc($_SESSION['buscarPorDesc'], $_SESSION['criterioBusqueda']); //Número de filas
+$totalPaginas = ceil($numeroFilas / $registrosPorPagina); //Número de páginas
+$primerRegistro = ($pagina - 1) * $registrosPorPagina; //Registro por el cual empieza a mostrar la página
+
+//Recojo los departamentos actuales en una variable de sesión para que permanezca 
+$_SESSION['aDepartamentos'] = Departamento::buscaDepartamentosPorDescripcion($_SESSION['buscarPorDesc'], $_SESSION['criterioBusqueda'], $primerRegistro, $registrosPorPagina); //Devuelve array de objetos
 
 $_SESSION['pagina'] = 'mtoDepartamentos';
 require_once $vistas['layout'];
--- view/vMtoDepartamentos.php
+++ view/vMtoDepartamentos.php
@@ -1,36 +1,20 @@
 <h1 class="titulo">MANTENIMIENTO DE DEPARTAMENTOS</h1>
 
 <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post"> 
-
     <div class="cajabusqueda" >
-        <input class="cajabuscar" type="text"  name="buscarPorDesc" placeholder="Introduzca búsqueda por descripción"
-               value="<?php echo $_REQUEST['buscarPorDesc']; ?>" >
-
+        <input class="cajabuscar" type="text"  name="buscarPorDesc" placeholder="Introduzca búsqueda por descripción" value="<?php echo $_SESSION['buscarPorDesc']; ?>" >
+        
         <input class='btn' type="submit" name="Buscar" value="Buscar">
         <input class='btn' type='submit' name='limpiarBuscar' value='Limpiar Búsqueda' ><br>
+        
         Criterios de búsqueda:
-
-        <input type="radio" name="criterioBusqueda" <?php
-        if ($_REQUEST['criterioBusqueda'] == "Baja") {
-            echo "checked";
-        }
-        ?> value="Baja">Baja
-
-        <input type="radio" name="criterioBusqueda" <?php
-        if ($_REQUEST['criterioBusqueda'] == "Alta") {
-            echo "checked";
-        }
-        ?> value="Alta">Alta
-
-        <input type="radio" name="criterioBusqueda" <?php
-        if ($_REQUEST['criterioBusqueda'] == "Todos") {
-            echo "checked";
-        }
-        ?> value="Todos">Todos
-
+        <input type="radio" name="criterioBusqueda" <?php if ($_SESSION['criterioBusqueda'] == "Baja") {echo "checked";}?> value="Baja">Baja
+        <input type="radio" name="criterioBusqueda" <?php if ($_SESSION['criterioBusqueda'] == "Alta") {echo "checked";}?> value="Alta">Alta
+        <input type="radio" name="criterioBusqueda" <?php if ($_SESSION['criterioBusqueda'] == "Todos") {echo "checked";}?> value="Todos">Todos
     </div>
 
     <br><br>
+    
     <div style="margin: 0 auto;text-align: center;">
         <input class='btn' type="submit" name="Importar" value="Importar" >
         <input class='btn' type="submit" name="Exportar" value="Exportar" >
@@ -48,60 +32,101 @@
             <td>Acciones</td>
         </tr>
         <?php
-        foreach ($aDepartamentos as $key => $Departamento) {
-            $key = $key + $primerRegistro; //Con esto asigno al key su valor actual más el de la propia página
-            
+        foreach ($_SESSION['aDepartamentos'] as $key => $Departamento) {
+            // $key = $key + $primerRegistro; //Con esto asigno al key su valor actual más el de la propia página
+
             $fechaBaja = $Departamento->getFechaBajaDepartamento();
+
             if ($fechaBaja == null) {
                 ?>
-                <tr style='background-color: #80FC7A'> <!-- Si la fechad e baja es nula se verá el fondo verde -->
-                <?php } else { ?>
-                <tr style='background-color: #FF4545'> <!-- Si la fechad e baja no es nula se verá el fondo rojo -->
-                <?php } ?>
-                <td><?php echo $Departamento->getCodDepartamento(); ?></td>  <!-- Muestro el actual código de departamento -->
-                <td><?php echo $Departamento->getDescDepartamento(); ?></td> <!-- Muestro la actual descripción del departamento -->                
-                <td><?php echo $Departamento->getFechaCreacionDepartamento(); ?></td> <!-- Muestro la actual descripción del departamento -->
-                <td><?php echo $Departamento->getVolumenDeNegocio(); ?> €</td> <!-- Muestro la actual descripción del departamento -->
-                <td>
-                    <div class='bocadillo'> <!-- Cada bloque como este produce un icono   -->
-                        <button  type='submit' name='Editar<?php echo $key ?>'><img src='webroot/images/editar.png'/></button>
-                        <span class='textoBocadillo'>Editar</span>
-                    </div>
-                    <div class='bocadillo'>
-                        <button  type='submit' name='Borrar<?php echo $key ?>'><img src='webroot/images/borrar.png'/></button>
-                        <span class='textoBocadillo'>Borrar</span>
-                    </div>
-
-                    <?php if ($fechaBaja != null) { ?><!-- Dependiendo de el estado del departamento muestro un botón u otro -->
-                        <div class='bocadillo'>
-                            <button  type='submit' name='Rehabilitar<?php echo $key ?>'><img src='webroot/images/alta.png'/></button>
-                            <span class='textoBocadillo'>Rehabilitación</span>
+                <tr style='background-color: #80FC7A'><!-- Si la fechad e baja es nula se verá el fondo verde -->
+                    <td><?php echo $Departamento->getCodDepartamento(); ?></td>  <!-- Muestro el actual código de departamento -->
+                    <td><?php echo $Departamento->getDescDepartamento(); ?></td> <!-- Muestro la actual descripción del departamento -->                
+                    <td><?php echo $Departamento->getFechaCreacionDepartamento(); ?></td> <!-- Muestro la actual descripción del departamento -->
+                    <td><?php echo $Departamento->getVolumenDeNegocio(); ?> €</td> <!-- Muestro la actual descripción del departamento -->
+                    <td>                     
+                        <div class='bocadillo'> <!-- Cada bloque como este produce un icono   -->
+                            <button  type='submit' name='Editar<?php echo $key ?>'><img src='webroot/images/editar.png'/></button>
+                            <span class='textoBocadillo'>Editar</span>
                         </div>
-                    <?php } else { ?>
+                        <div class='bocadillo'>
+                            <button  type='submit' name='Borrar<?php echo $key ?>'><img src='webroot/images/borrar.png'/></button>
+                            <span class='textoBocadillo'>Borrar</span>
+                        </div>
                         <div class='bocadillo'>
                             <button  type='submit' name='Baja<?php echo $key ?>'><img src='webroot/images/baja.png'/></button>
                             <span class='textoBocadillo'>Baja lógica</span>
                         </div>
-                    <?php } ?>
-                </td>
-            </tr>
-        <?php } ?>
+                    </td>
+                </tr>
 
-    </table>
+            <?php } else { ?>
+                <tr style='background-color: #FF4545'> <!-- Si la fechad e baja no es nula se verá el fondo rojo -->
+                    <td><?php echo $Departamento->getCodDepartamento(); ?></td>  <!-- Muestro el actual código de departamento -->
+                    <td><?php echo $Departamento->getDescDepartamento(); ?></td> <!-- Muestro la actual descripción del departamento -->                
+                    <td><?php echo $Departamento->getFechaCreacionDepartamento(); ?></td> <!-- Muestro la actual descripción del departamento -->
+                    <td><?php echo $Departamento->getVolumenDeNegocio(); ?> €</td> <!-- Muestro la actual descripción del departamento -->
+                    <td>                                             
+                        <div class='bocadillo'>
+                            <button  type='submit' name='Borrar<?php echo $key ?>'><img src='webroot/images/borrar.png'/></button>
+                            <span class='textoBocadillo'>Borrar</span>
+                        </div>
+                        <div class='bocadillo'>
+                            <button  type='submit' name='Rehabilitar<?php echo $key ?>'><img src='webroot/images/alta.png'/></button>
+                            <span class='textoBocadillo'>Rehabilitación</span>
+                        </div>
+                    </td>
+                </tr>
+
+            <?php } ?>
+    <?php } ?>
+        </table>
+
+    <br>
     <div class="paginacion">
-        <a href="?pag=1">Primera</a>
-        <a href="?pag=<?php if($_GET[pag]>1){echo (($_GET[pag])-1);}else{ echo "1";}?>"><</a>
+
         <?php
-        //Array para recorrer las páginas y poder crear la paginación
-        //Muestro los números de página creando links que podifican la página
-        for ($i = 1; $i <= $totalPaginas; $i++) {
-            echo " <a href='?pag=" . $i . "'>" . $i . "</a> "; //Link que al pulsarlo almacena una variable en la URL llamada página con el valor de páginas
+        if ($totalPaginas != 0) {//Si existe más de una página, es decir si existen departamentos
+
+            /*             * ******************BOTÓN DE PRIMERA PÁGINA************************** */
+            if ($totalPaginas >= 1 && $pagina != 1) { //Si hay más de uyna página se muestra el botón de primero
+                echo "<a href='?pag=1&?buscarPorDesc" . $_SESSION['buscarPorDesc'] . "&?criterioBusqueda=" . $_SESSION['criterioBusqueda'] . "'>Primera</a>";
+            } else {
+                echo " <a href='' id='inactivo'>Primera</a> "; //Link inactivo para mantener la apariencia
+            }
+
+            /*             * ******************BOTÓN DE ATRASO DE PÁGINA************************** */
+            if ($pagina > 1) {//Si la página es mayor que uno muestro el botón de siguiente página
+                echo " <a href='?pag=" . (($_GET[pag]) - 1) . "&?buscarPorDesc=" . $_SESSION['buscarPorDesc'] . "&?criterioBusqueda=" . $_SESSION['criterioBusqueda'] . "'>⬅</a> "; //Link que al pulsarlo almacena una variable en la URL llamada página con el valor de páginas
+            } else {
+                echo " <a href='' id='inactivo'>⬅</a> "; //Link inactivo para mantener la apariencia
+            }
+
+            /*             * ******************BOTÓN DE AVANCE DE PÁGINA************************** */
+            if ($pagina < $totalPaginas) {
+                if (($_GET[pag] == "")) {//Cuando la variable get no está definida en la URL, es decir la primera vez que se entra a la página
+                    //Se suman 2 páginas para que vaya a ala segunda y no a la primera
+                    echo " <a href='?pag=" . (($_GET[pag]) + 2) . "&?buscarPorDesc=" . $_SESSION['buscarPorDesc'] . "&?criterioBusqueda=" . $_SESSION['criterioBusqueda'] . "'>➡</a> "; //Link que al pulsarlo almacena una variable en la URL llamada página con el valor de páginas
+                } else {
+                    echo " <a href='?pag=" . (($_GET[pag]) + 1) . "&?buscarPorDesc=" . $_SESSION['buscarPorDesc'] . "&?criterioBusqueda=" . $_SESSION['criterioBusqueda'] . "'>➡</a> "; //Link que al pulsarlo almacena una variable en la URL llamada página con el valor de páginas
+                }
+            } else {
+                echo " <a href='' id='inactivo'>➡</a> "; //Link inactivo para mantener la apariencia
+            }
+
+            /*             * ******************BOTÓN DE ÚLTIMA PÁGINA************************** */
+            if ($totalPaginas > 1 && $pagina != $totalPaginas) { //Si hay más de uyna página se muestra el botón de último y no es la última
+                echo "<a href='?pag=" . $totalPaginas . "'>Última</a>";
+            } else {
+                echo " <a href='' id='inactivo'>Última</a> "; //Link inactivo para mantener la apariencia
+            }
+
+            /*             * ******************INFORMACIÓN PÁGINA ACTUAL Y TOTAL************************** */
+            echo "<br>Página " . $pagina . " de " . $totalPaginas;
+        } else {//Si no existen resultados se muestra un mensaje de error
+            echo "Parece que el departamento que busca no existe...";
         }
-        
         ?>
-        <a href="?pag=<?php if($_GET[pag]<$totalPaginas){echo (($_GET[pag])+1);}else{echo $totalPaginas;}?>">></a>
-        <a href="?pag=<?php echo $totalPaginas ?>">Ultima</a>
-        <br>Página  <?php echo $pagina ?> de <?php echo $totalPaginas?>
     </div>
 </form>
 
